44f09e8fff45a559fd3e3b14047c247e6b315565
NGC13009 <ngc1300@126.com>
2024-10-11 11:26:39 +0800

    Update Models.py make PE with torch instead of numpy
    Update Models.py make PE with torch instead of numpy.

--- transformer/Models.py
+++ transformer/Models.py
@@ -29,16 +29,10 @@ class PositionalEncoding(nn.Module):
         self.register_buffer('pos_table', self._get_sinusoid_encoding_table(n_position, d_hid))
 
     def _get_sinusoid_encoding_table(self, n_position, d_hid):
-        ''' Sinusoid position encoding table '''
-        # TODO: make it with torch instead of numpy
-
-        def get_position_angle_vec(position):
-            return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)]
-
-        sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)])
-        sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
-        sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
-
+        sinusoid = (torch.arange(0, n_position, 1).unsqueeze(0).mT) / (10000.0 ** torch.tensor([2 * (hid_j // 2) / d_hid for hid_j in range(d_hid)]))
+        sinusoid_table = torch.zeros(n_position, d_hid)
+        sinusoid_table[:, 0::2] = torch.sin(sinusoid[:, 0::2])  # dim 2i
+        sinusoid_table[:, 1::2] = torch.cos(sinusoid[:, 1::2])  # dim 2i+1
         return torch.FloatTensor(sinusoid_table).unsqueeze(0)
 
     def forward(self, x):
